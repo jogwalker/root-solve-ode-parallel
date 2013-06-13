7bba7c0f15319e975b18f4269032568d9aaf456e
Josephine Walker <jogwalker@gmail.com>
2013-06-13 18:33:39 +1000

    change memory in .sh, add to .gitignore, and started file to analyze results

---
+++ analyze/run_ode_with_success_params.R
@@ -0,0 +1,6 @@
+# load the data
+
+load("C:/Users/Josephine/Dropbox/Dropbox Documents/Bristol_PhD/R/qualitative modeling/results/results_536success.Rdata")
+
+result <- output$result
+parameters <- output$parameters
--- run_solve_simulations.sh
+++ run_solve_simulations.sh
@@ -4,14 +4,14 @@
 #$ -S /bin/bash
 #$ -o job_reports/
 #$ -e job_reports/
-#$ -t 1-1000
-#$ -l h_vmem=5G
+#$ -t 1001-10000
+#$ -l h_vmem=2G
 
 if [ -n "${1}" ]; then
         SGE_TASK_ID=${1}
 fi
 i=${SGE_TASK_ID}
 
-outdir="/clusterdata/uqgheman/jummy/qualitative-modeling/results/"
+outdir="/clusterdata/uqgheman/jummy/qualitative-modeling/results/test/"
 
 R --no-save --args ${outdir} ${i} < /clusterdata/uqgheman/jummy/qualitative-modeling/R/run_solve_simulations.R
